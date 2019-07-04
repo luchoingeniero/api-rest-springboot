cdcf1f1fa46877c95c8f9bcb4298e7188ced8e71
luis.galeano <luchoingeniero@gmail.com>
2019-07-04 16:37:26 -0500

    primer Commit

---
+++ src/main/java/com/lgaleano/ApiRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.lgaleano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestExampleApplication.class, args);
+	}
+
+}
